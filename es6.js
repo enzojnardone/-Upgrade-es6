18977f5b5ad5a099a1fc8f0b300d79eb9075fd46
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-05-18 00:15:57 +0200

    Iteración 4, 4.1

--- es6.js
+++ es6.js
@@ -110,3 +110,38 @@ const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
 let colorsCopy = [...colors];
 colorsCopy.splice(1, 1);
 console.log(colorsCopy);
+
+// **Iteración #4: Map**
+// 4.1 Dado el siguiente array, devuelve un array con sus nombres 
+// utilizando .map().
+
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+let userNames = users.map(user => user.name);
+console.log(userNames);
+
+// 4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
+// de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
+// empiece por 'A'.
+
+// const users = [
+// 	{id: 1, name: 'Abel'},
+// 	{id:2, name: 'Julia'},
+// 	{id:3, name: 'Pedro'},
+// 	{id:4, name: 'Amanda'}
+// ];
+
+// 4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+// de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
+// cuando el valor de la propiedad isVisited = true.
+// const cities = [
+// 	{isVisited:true, name: 'Tokyo'}, 
+// 	{isVisited:false, name: 'Madagascar'},
+// 	{isVisited:true, name: 'Amsterdam'}, 
+// 	{isVisited:false, name: 'Seul'}
+// ];
