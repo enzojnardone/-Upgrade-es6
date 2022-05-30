b78cf59ec93e98aed3c8775a4a50fdd60c9b1e01
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-05-30 23:52:31 +0200

    iteracion 1 a 7

--- es6.js
+++ es6.js
@@ -289,3 +289,43 @@ console.log(findmMutation);
 
 const newAlien = {...findAlien, ...findmMutation}
 console.log(newAlien);
+
+// Iteración #7: Reduce
+
+// 7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+// los alumnos usando la función .reduce().
+
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+
+const sum = exams.reduce((acc, student) => acc+student.score,0);
+console.log(sum);
+
+// 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+// alumnos que esten aprobados usando la función .reduce().
+
+const approvedStudents = exams.reduce((acc, students) => {
+    if(students.score >= 5) {
+        acc+=students.score;
+    }
+    return acc;
+},0);
+
+console.log(approvedStudents);
+
+// 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
+
+let count = 0;
+const mediaScore = exams.reduce((acc, students) => acc+students.score,0) / exams.length;
+
+console.log(mediaScore);
