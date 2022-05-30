566c24a99aae4f89de7cac77455ade0c514baba5
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-05-30 23:11:30 +0200

    iteración 5 - 5.6

---
+++ es6-5-6.js
@@ -0,0 +1,21 @@
+// 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+// introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+
+const streamers = [
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+];
+
+const streamer = document.querySelector(".searching");
+streamer.addEventListener("input", (event) => {
+  const input = event.target.value.toLowerCase().trim();
+  const filtered = streamers.filter((streamer) => {
+    return streamer.name.toLowerCase().includes(input);
+  });
+
+  console.log(filtered);
+});
